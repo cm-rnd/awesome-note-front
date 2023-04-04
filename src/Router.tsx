d5a6e153c950e74e8d886e0d28c8a7f12839c174
sunghoon <shun1423@naver.com>
2023-04-04 15:57:12 +0900

    [refactor] Router sign부분 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import { SignIn } from "./components/Sign/SignIn";
+import { SignUp } from "./components/Sign/SignUp";
 import Home from "./pages/Home/Home";
 import Note from "./pages/Home/Switch/Note";
 
 import NotePage from "./pages/Home/Switch/NotePage";
-import { SignIn, SignUp } from "./pages/Sign/SignComponent";
 
 import Sign from "./pages/Sign/SignPage";
 
