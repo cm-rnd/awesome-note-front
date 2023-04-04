7bfab8ed31f32f535e3599e815b5a0be31c28744
sunghoon <shun1423@naver.com>
2023-04-04 18:21:57 +0900

    [refact]정규식으로 이메일 형식 추가

--- src/components/Sign/SignUp.tsx
+++ src/components/Sign/SignUp.tsx
@@ -87,6 +87,11 @@ export function SignUp() {
         <SignInput
           {...register("email", {
             required: "Email is required",
+            pattern: {
+              value:
+                /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+              message: "이메일 형식에 맞지 않습니다.",
+            },
           })}
           placeholder="Email"
         />
