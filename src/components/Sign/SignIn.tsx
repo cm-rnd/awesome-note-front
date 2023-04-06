aa2d3fbd0860e1374a3ea88839a5a112221139ef
sunghoon <shun1423@naver.com>
2023-04-06 13:48:07 +0900

    [feat] 로그인 api 연결및 기능 구현

--- src/components/Sign/SignIn.tsx
+++ src/components/Sign/SignIn.tsx
@@ -5,18 +5,12 @@ import {
   SignForm,
   SignInput,
 } from "../StyleComponent/SignStyle";
+import { useNavigate } from "react-router";
+import axios from "axios";
 
-interface FormData {
-  errors: {
-    email: {
-      message: string;
-    };
-  };
-  name: string;
-  userName: string;
-  email: string;
+interface LoginFormData {
+  loginId: string;
   password: string;
-  passwordCheck: string;
   extraError?: string;
 }
 
@@ -26,16 +20,49 @@ export function SignIn() {
     handleSubmit,
     formState: { errors },
     setError,
-  } = useForm<FormData>();
+  } = useForm<LoginFormData>();
+  const navigate = useNavigate();
+  const LoginPost = (data: LoginFormData) => {
+    console.log(data);
+    axios
+      .post("http://192.168.159.42:20000/api/v1/login", data, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res);
+        console.log("res.data.data.loginId :: ", res.data.data.loginId);
+        //console.log(res.headers.getAuthorization);
+
+        if (res.data.data.loginId === undefined) {
+          alert("id가 없습니다.");
+        } else if (res.data.data.password === null) {
+          alert("비밀번호가 일치하지 않습니다.");
+        } else if (res.data.data.loginId === data.loginId) {
+          sessionStorage.setItem("login_id", res.data.data.loginId);
+          sessionStorage.setItem("role", res.data.data.role);
+          sessionStorage.setItem("nickname", res.data.data.nickname);
+          console.log("로그인 완료");
+          navigate(`/`);
+        }
+        // document.location.href = "/";
+      })
+      .catch((error) => {
+        window.alert("로그인 실패");
+        console.log(error);
+      });
+
+    setError("extraError", { message: "서버 닫힘" });
+  };
+  console.log(errors);
 
   return (
     <div>
-      <SignForm>
+      <SignForm onSubmit={handleSubmit(LoginPost)}>
         <SignInput
-          {...register("userName", { required: true, minLength: 1 })}
-          placeholder="UserName"
+          {...register("loginId", { required: true, minLength: 1 })}
+          placeholder="Login ID"
         />
-        <span>{errors?.userName?.message}</span>
+        <span>{errors?.loginId?.message}</span>
         <SignInput
           {...register("password", { required: true, minLength: 1 })}
           placeholder="Password"
