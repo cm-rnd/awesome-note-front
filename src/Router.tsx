57d7805a2531593d614e3fe53c2c17b09eb347e8
sunghoon <shun1423@naver.com>
2023-04-19 15:18:47 +0900

    [refact] Router 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -18,10 +18,6 @@ const router = createBrowserRouter([
 
     children: [
       {
-        path: "",
-        element: <Home />,
-      },
-      {
         path: "/page/:teamId",
         element: <RefTeamNotesPage />,
       },
